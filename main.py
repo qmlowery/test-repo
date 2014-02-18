a9b012b6c1619cefc7bf8a2a05f443c99ba56566
ethan <mata.ethan@gmail.com>
2014-02-18 17:35:48 -0600

    Added comments
    Summary: 	Added comments for readability
    
    Test Plan: 	Run it, most likely
    
    Reviewers: matt, quentin, rob, victor
    
    Differential Revision: http://68.119.91.146:5667/D5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def main():
-	print(“Hello, world”)
+def main(): // Defines a main function, named 'main'.
+	print(“Hello, world”) // Prints "Hello, world".
 
-main();
+main();  // Runs the main function, which is the function named 'main'.
