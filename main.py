372c96a61c6886f541c04b3d949be12a8fe5db13
Quentin Lowery <quentinlowery@gmail.com>
2014-03-04 19:07:49 -0600

    Changed some text.
    Added text to puppycat thing.
    Added gibberish comment to main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ def main(): // Defines a main function, named 'main'.
 	print(“Hello, world”) // Prints "Hello, world".
 
 main();  // Runs the main function, which is the function named 'main'.
+//HERLJARGJENGSFPTOARGJLBKGNSFAJRLJTGR
